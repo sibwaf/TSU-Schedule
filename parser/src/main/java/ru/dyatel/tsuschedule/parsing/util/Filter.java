f2a0228d0560f392c31f52b2429534643a994b5f
sibwaf <7131069+sibwaf@users.noreply.github.com>
2015-10-03 17:04:43 +0300

    Add JavaDoc for util classes

--- parser/src/main/java/ru/dyatel/tsuschedule/parsing/util/Filter.java
+++ parser/src/main/java/ru/dyatel/tsuschedule/parsing/util/Filter.java
@@ -1,7 +1,21 @@
 package ru.dyatel.tsuschedule.parsing.util;
 
+/**
+ * A Filter to use in the {@link ru.dyatel.tsuschedule.parsing.util.IterableFilter} instance.
+ * <p>
+ * Multiple filters can be applied.
+ * </p>
+ *
+ * @see ru.dyatel.tsuschedule.parsing.util.IterableFilter#apply(ru.dyatel.tsuschedule.parsing.util.Filter)
+ */
 public interface Filter<T> {
 
+    /**
+     * Checks if given object should be added to the filtered collection.
+     *
+     * @param obj object to check
+     * @return true if object should remain, false otherwise
+     */
     boolean accept(T obj);
 
 }
