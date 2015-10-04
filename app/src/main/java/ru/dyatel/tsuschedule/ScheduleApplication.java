802de941652ae97d52d717709e7f1517dcbba745
dya-tel <dya-tel@yandex.ru>
2015-10-04 23:38:48 +0300

    Add ACRA crash reporter

---
+++ app/src/main/java/ru/dyatel/tsuschedule/ScheduleApplication.java
@@ -0,0 +1,20 @@
+package ru.dyatel.tsuschedule;
+
+import android.app.Application;
+import org.acra.ACRA;
+import org.acra.annotation.ReportsCrashes;
+
+@ReportsCrashes(
+        formUri = BuildConfig.ACRA_BACKEND
+)
+public class ScheduleApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize ACRA
+        ACRA.init(this);
+    }
+
+}
