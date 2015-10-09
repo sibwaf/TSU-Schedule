ea4d796c5777975505357feadb19146715fb0864
dya-tel <dya-tel@yandex.ru>
2015-10-09 19:55:28 +0300

    Add more tests

--- parser/src/test/java/ru/dyatel/tsuschedule/parsing/ParserTest.java
+++ parser/src/test/java/ru/dyatel/tsuschedule/parsing/ParserTest.java
@@ -12,6 +12,12 @@ import static org.junit.Assert.assertTrue;
 
 public class ParserTest {
 
+    private Filter<Lesson> evenParity = new Filter<Lesson>() {
+        @Override
+        public boolean accept(Lesson obj) {
+            return obj.getParity().equals(Parity.EVEN) || obj.getParity().equals(Parity.BOTH);
+        }
+    };
     private Filter<Lesson> oddParity = new Filter<Lesson>() {
         @Override
         public boolean accept(Lesson obj) {
@@ -68,6 +74,62 @@ public class ParserTest {
         assertTrue(containsDiscipline(result, "Кураторский час"));
         assertTrue(containsDiscipline(result, "Л:Физическая культура"));
         assertTrue(containsDiscipline(result, "Л:Алгебра и аналитическая геометрия"));
+    }
+
+    @Test
+    public void testGetLessonsFridayEven() throws Exception {
+        IterableFilter<Lesson> filter = new IterableFilter<Lesson>();
+        filter.apply(evenParity);
+        filter.apply(new Filter<Lesson>() {
+            @Override
+            public boolean accept(Lesson obj) {
+                return obj.getWeekday().equals("пятница");
+            }
+        });
+        filter.apply(firstSubgroup);
+
+        Set<Lesson> result = filter.filter(lessons);
+        assertEquals(4, result.size());
+        assertTrue(containsDiscipline(result, "Л:Физическая культура"));
+        assertTrue(containsDiscipline(result, "Лаб:Языки и методы программирования"));
+        Lesson lab = null;
+        for (Lesson l : result) {
+            if (l.getDiscipline().equals("Лаб:Языки и методы программирования")) {
+                lab = l;
+                break;
+            }
+        }
+        result.remove(lab);
+        assertTrue(containsDiscipline(result, "Лаб:Языки и методы программирования"));
+        assertTrue(containsDiscipline(result, "Пр:Математическая составляющая естественнонаучных дисциплин"));
+    }
+
+    @Test
+    public void testGetLessonsFridayOdd() throws Exception {
+        IterableFilter<Lesson> filter = new IterableFilter<Lesson>();
+        filter.apply(oddParity);
+        filter.apply(new Filter<Lesson>() {
+            @Override
+            public boolean accept(Lesson obj) {
+                return obj.getWeekday().equals("пятница");
+            }
+        });
+        filter.apply(firstSubgroup);
+
+        Set<Lesson> result = filter.filter(lessons);
+        assertEquals(4, result.size());
+        assertTrue(containsDiscipline(result, "Л:Физическая культура"));
+        assertTrue(containsDiscipline(result, "Лаб:Языки и методы программирования"));
+        Lesson lab = null;
+        for (Lesson l : result) {
+            if (l.getDiscipline().equals("Лаб:Языки и методы программирования")) {
+                lab = l;
+                break;
+            }
+        }
+        result.remove(lab);
+        assertTrue(containsDiscipline(result, "Лаб:Языки и методы программирования"));
+        assertTrue(containsDiscipline(result, "Пр:Математический анализ"));
     }
 
     private boolean containsDiscipline(Set<Lesson> lessons, String discipline) {
