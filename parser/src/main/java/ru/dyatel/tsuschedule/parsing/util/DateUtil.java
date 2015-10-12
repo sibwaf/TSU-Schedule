7683f323a6844d0f9d55d3c264dee817108eea45
sibwaf <7131069+sibwaf@users.noreply.github.com>
2015-10-12 23:19:02 +0300

    Add util for getting week parity

---
+++ parser/src/main/java/ru/dyatel/tsuschedule/parsing/util/DateUtil.java
@@ -0,0 +1,28 @@
+package ru.dyatel.tsuschedule.parsing.util;
+
+import hirondelle.date4j.DateTime;
+import ru.dyatel.tsuschedule.parsing.Parity;
+
+/**
+ * An utility class to manipulate date.
+ */
+public class DateUtil {
+
+    /**
+     * Finds out if the week of the provided date is
+     * even or odd relative to the beginning of the academic year.
+     *
+     * @param now a date to check
+     * @return {@link ru.dyatel.tsuschedule.parsing.Parity#EVEN} if provided date
+     * is on a even week, {@link ru.dyatel.tsuschedule.parsing.Parity#ODD} otherwise
+     */
+    public static Parity getWeekParity(DateTime now) {
+        int startYear = now.getYear() - (now.getMonth() < 9 ? 1 : 0);
+        DateTime start = DateTime.forDateOnly(startYear, 9, 1);
+
+        int weekDay = (start.getWeekDay() + 6) % 7; // Convert Sunday..Saturday -> Monday..Sunday
+
+        return now.getWeekIndex(start.minusDays(weekDay - 1)) % 2 == 0 ? Parity.EVEN : Parity.ODD;
+    }
+
+}
---
+++ parser/src/test/java/ru/dyatel/tsuschedule/parsing/util/DateUtilTest.java
@@ -0,0 +1,53 @@
+package ru.dyatel.tsuschedule.parsing.util;
+
+import hirondelle.date4j.DateTime;
+import org.junit.Test;
+import ru.dyatel.tsuschedule.parsing.Parity;
+
+import static org.junit.Assert.assertEquals;
+
+public class DateUtilTest {
+
+    @Test
+    public void testOddWeeks() {
+        // September
+        assertEquals(Parity.ODD, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 9, 1)));
+        assertEquals(Parity.ODD, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 9, 6)));
+        assertEquals(Parity.ODD, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 9, 14)));
+        assertEquals(Parity.ODD, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 9, 17)));
+        assertEquals(Parity.ODD, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 9, 20)));
+
+        // October
+        assertEquals(Parity.ODD, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 10, 1)));
+        assertEquals(Parity.ODD, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 10, 4)));
+        assertEquals(Parity.ODD, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 10, 12)));
+        assertEquals(Parity.ODD, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 10, 15)));
+        assertEquals(Parity.ODD, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 10, 18)));
+
+        // January
+        assertEquals(Parity.ODD, DateUtil.getWeekParity(DateTime.forDateOnly(2016, 1, 4)));
+        assertEquals(Parity.ODD, DateUtil.getWeekParity(DateTime.forDateOnly(2016, 1, 7)));
+        assertEquals(Parity.ODD, DateUtil.getWeekParity(DateTime.forDateOnly(2016, 1, 10)));
+    }
+
+    @Test
+    public void testEvenWeeks() {
+        // September
+        assertEquals(Parity.EVEN, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 9, 7)));
+        assertEquals(Parity.EVEN, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 9, 10)));
+        assertEquals(Parity.EVEN, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 9, 13)));
+
+        // October
+        assertEquals(Parity.EVEN, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 10, 5)));
+        assertEquals(Parity.EVEN, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 10, 8)));
+        assertEquals(Parity.EVEN, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 10, 11)));
+        assertEquals(Parity.EVEN, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 10, 19)));
+        assertEquals(Parity.EVEN, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 10, 22)));
+        assertEquals(Parity.EVEN, DateUtil.getWeekParity(DateTime.forDateOnly(2015, 10, 25)));
+
+        // January
+        assertEquals(Parity.EVEN, DateUtil.getWeekParity(DateTime.forDateOnly(2016, 1, 1)));
+        assertEquals(Parity.EVEN, DateUtil.getWeekParity(DateTime.forDateOnly(2016, 1, 3)));
+    }
+
+}
