3e53078f0918777883d8e74edb22b2882732e4f8
sibwaf <7131069+sibwaf@users.noreply.github.com>
2015-10-17 20:08:36 +0300

    Remove unused menu

--- app/src/main/java/ru/dyatel/tsuschedule/MainActivity.java
+++ app/src/main/java/ru/dyatel/tsuschedule/MainActivity.java
@@ -8,8 +8,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import hirondelle.date4j.DateTime;
 import ru.dyatel.tsuschedule.data.DataFragment;
 import ru.dyatel.tsuschedule.parsing.util.DateUtil;
@@ -78,27 +76,6 @@ public class MainActivity extends AppCompatActivity {
                 .apply();
 
         dataFragment.saveData();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
