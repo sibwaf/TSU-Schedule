408557fce5c883637c5f45cfb092e5e995c12398
sibwaf <7131069+sibwaf@users.noreply.github.com>
2016-02-27 15:59:01 +0300

    Clean up a bit ParityFilter and SubgroupFilter

--- parser/src/main/kotlin/ru/dyatel/tsuschedule/parsing/SubgroupFilter.kt
+++ parser/src/main/kotlin/ru/dyatel/tsuschedule/parsing/SubgroupFilter.kt
@@ -2,13 +2,7 @@ package ru.dyatel.tsuschedule.parsing
 
 import ru.dyatel.tsuschedule.util.Filter
 
-class SubgroupFilter(subgroup: Int) : Filter<Lesson> {
-
-    private val subgroup: Int;
-
-    init {
-        this.subgroup = subgroup;
-    }
+class SubgroupFilter(private val subgroup: Int) : Filter<Lesson> {
 
     override fun accept(obj: Lesson?): Boolean =
             obj!!.subgroup == 0 || obj.subgroup == subgroup
