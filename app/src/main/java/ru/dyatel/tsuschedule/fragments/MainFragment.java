764a5fe869d5bec0156391d5b641db747dce8636
SibWaf <dya-tel@yandex.ru>
2016-02-27 19:42:58 +0300

    Move navigation drawer into helper classes

--- app/src/main/java/ru/dyatel/tsuschedule/fragments/MainFragment.java
+++ app/src/main/java/ru/dyatel/tsuschedule/fragments/MainFragment.java
@@ -1,34 +1,19 @@
 package ru.dyatel.tsuschedule.fragments;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
 import hirondelle.date4j.DateTime;
-import org.solovyev.android.views.llm.LinearLayoutManager;
-import ru.dyatel.tsuschedule.MainActivity;
 import ru.dyatel.tsuschedule.ParityReference;
 import ru.dyatel.tsuschedule.R;
-import ru.dyatel.tsuschedule.data.DataFragment;
-import ru.dyatel.tsuschedule.layout.MenuButtonAdapter;
+import ru.dyatel.tsuschedule.layout.NavigationDrawerHandler;
 import ru.dyatel.tsuschedule.layout.WeekFragmentPagerAdapter;
 import ru.dyatel.tsuschedule.parsing.DateUtil;
 
@@ -36,64 +21,7 @@ import java.util.TimeZone;
 
 public class MainFragment extends Fragment {
 
-    private static final String DRAWER_LEARNED_KEY = "drawer_learned";
-
-    private DataFragment dataFragment;
-
-    private ActionBarDrawerToggle toggle;
-
-    private EditText groupIndexText;
-    private Spinner subgroupSpinner;
-
-    private void initDrawer(DrawerLayout layout, Toolbar toolbar, final DataFragment data) {
-        groupIndexText.setText(data.getGroup());
-        subgroupSpinner.setSelection(data.getSubgroup() - 1);
-        // subgroup 1 has (1 - 1) position in string array, etc.
-
-        Activity activity = getActivity();
-
-        toggle = new ActionBarDrawerToggle(
-                activity,
-                layout,
-                toolbar,
-                R.string.navigation_drawer_open,
-                R.string.navigation_drawer_close
-        ) {
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                super.onDrawerClosed(drawerView);
-                //Activity activity = getActivity();
-
-                // Hide the keyboard
-                /*InputMethodManager imm =
-                        (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(groupIndex.getWindowToken(), 0);*/
-                groupIndexText.onEditorAction(EditorInfo.IME_ACTION_DONE);
-
-                // Save new group and subgroup in DataFragment
-                data.setGroup(groupIndexText.getText().toString());
-                data.setSubgroup(subgroupSpinner.getSelectedItemPosition() + 1);
-            }
-        };
-        layout.addDrawerListener(toggle);
-
-        // Open drawer if user had never seen it
-        SharedPreferences preferences =
-                activity.getSharedPreferences(MainActivity.PREFERENCES_FILE, Context.MODE_PRIVATE);
-        boolean seenDrawer = preferences.getBoolean(DRAWER_LEARNED_KEY, false);
-        if (!seenDrawer) {
-            layout.openDrawer(Gravity.LEFT);
-            preferences.edit()
-                    .putBoolean(DRAWER_LEARNED_KEY, true)
-                    .apply();
-        }
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        dataFragment = (DataFragment) getFragmentManager().findFragmentByTag(DataFragment.TAG);
-    }
+    private NavigationDrawerHandler navigationDrawerHandler;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -101,52 +29,9 @@ public class MainFragment extends Fragment {
 
         FragmentManager fm = getFragmentManager();
 
-        // -----------------------
-        // Navigation drawer start
-
-        // Show current parity string in the navigation drawer
-        ((TextView) root.findViewById(R.id.current_parity)).setText(ParityReference.getStringFromParity(
-                DateUtil.getWeekParity(DateTime.now(TimeZone.getDefault()))
-        ));
-
-        groupIndexText = (EditText) root.findViewById(R.id.group_index);
-
-        // Manage subgroup spinner
-        subgroupSpinner = (Spinner) root.findViewById(R.id.subgroup);
-        ArrayAdapter<CharSequence> subgroupAdapter = ArrayAdapter.createFromResource(
-                getActivity(),
-                R.array.subgroups,
-                android.R.layout.simple_spinner_item
+        navigationDrawerHandler = new NavigationDrawerHandler(
+                getActivity(), (DrawerLayout) root
         );
-        subgroupAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        subgroupSpinner.setAdapter(subgroupAdapter);
-
-        // Manage the app menu
-        RecyclerView appMenu = (RecyclerView) root.findViewById(R.id.menu_list);
-        appMenu.setLayoutManager(new LinearLayoutManager(root.getContext()));
-        MenuButtonAdapter menuAdapter = new MenuButtonAdapter();
-        menuAdapter.addMenuEntry(new MenuButtonAdapter.MenuEntry(
-                R.drawable.ic_settings, R.string.action_settings,
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        getFragmentManager().beginTransaction()
-                                .replace(R.id.content_fragment, new SettingsFragment())
-                                .addToBackStack("settings_opened")
-                                .commit();
-                    }
-                })
-        );
-        appMenu.setAdapter(menuAdapter);
-
-        initDrawer(
-                (DrawerLayout) root,
-                (Toolbar) container.findViewById(R.id.toolbar),
-                dataFragment
-        );
-
-        // Navigation drawer end
-        // ---------------------
 
         // Set up the ViewPager with the sections adapter and select current parity tab
         ViewPager viewPager = (ViewPager) root.findViewById(R.id.pager);
@@ -167,13 +52,13 @@ public class MainFragment extends Fragment {
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        toggle.onConfigurationChanged(newConfig);
+        navigationDrawerHandler.onConfigurationChanged(newConfig);
     }
 
     @Override
     public void onViewStateRestored(Bundle savedInstanceState) {
         super.onViewStateRestored(savedInstanceState);
-        toggle.syncState();
+        navigationDrawerHandler.syncState();
     }
 
 }
---
+++ app/src/main/kotlin/ru/dyatel/tsuschedule/layout/NavigationDrawerHandler.kt
@@ -0,0 +1,112 @@
+package ru.dyatel.tsuschedule.layout
+
+import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
+import android.content.res.Configuration
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.view.Gravity
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import hirondelle.date4j.DateTime
+import ru.dyatel.tsuschedule.MainActivity
+import ru.dyatel.tsuschedule.ParityReference
+import ru.dyatel.tsuschedule.R
+import ru.dyatel.tsuschedule.data.DataFragment
+import ru.dyatel.tsuschedule.parsing.DateUtil
+import java.util.TimeZone
+import android.R as AR
+
+class NavigationDrawerHandler(
+        activity: Activity,
+        layout: DrawerLayout
+) {
+
+    private val toggle: ActionBarDrawerToggle
+
+    init {
+        val dataFragment = activity.fragmentManager.findFragmentByTag(DataFragment.TAG) as DataFragment
+
+        val groupIndexEdit = layout.findViewById(R.id.group_index) as EditText
+        val subgroupSpinner = layout.findViewById(R.id.subgroup) as Spinner
+
+        // Initialize drawer layout
+        manageLayout(
+                layout.findViewById(R.id.current_parity) as TextView,
+                groupIndexEdit, subgroupSpinner, dataFragment,
+                activity
+        )
+
+        // Initialize app menu
+        createAppMenu(
+                activity.fragmentManager, layout.context,
+                layout.findViewById(R.id.menu_list) as RecyclerView
+        )
+
+        // Manage the drawer toggle
+        toggle = object : ActionBarDrawerToggle(
+                activity, layout, activity.findViewById(R.id.toolbar) as Toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        ) {
+            override fun onDrawerClosed(drawerView: View?) {
+                // Save new group and subgroup
+                dataFragment.group = groupIndexEdit.text.toString()
+                dataFragment.subgroup = subgroupSpinner.selectedItemPosition + 1
+
+                super.onDrawerClosed(drawerView)
+            }
+        }
+        layout.addDrawerListener(toggle)
+
+        // Open the drawer if app is launched for the first time
+        openDrawerForTheFirstTime(
+                activity.getSharedPreferences(MainActivity.PREFERENCES_FILE, Context.MODE_PRIVATE),
+                layout
+        )
+    }
+
+    fun onConfigurationChanged(config: Configuration) = toggle.onConfigurationChanged(config)
+    fun syncState() = toggle.syncState()
+
+}
+
+private fun manageLayout(
+        currentParity: TextView,
+        groupIndexEdit: EditText,
+        subgroupSpinner: Spinner,
+        dataFragment: DataFragment,
+        context: Context
+) {
+    // Show current parity string in the navigation drawer
+    currentParity.text = ParityReference.getStringFromParity(DateUtil.getWeekParity(
+            DateTime.now(TimeZone.getDefault())
+    ))
+
+    // Put current group index into the group index editor
+    groupIndexEdit.setText(dataFragment.group)
+
+    // Manage the subgroup spinner
+    val spinnerAdapter = ArrayAdapter.createFromResource(
+            context, R.array.subgroups, AR.layout.simple_spinner_item
+    )
+    spinnerAdapter.setDropDownViewResource(AR.layout.simple_spinner_dropdown_item)
+    subgroupSpinner.adapter = spinnerAdapter
+    subgroupSpinner.setSelection(dataFragment.subgroup - 1)
+}
+
+const val DRAWER_LEARNED_KEY = "drawer_learned"
+private fun openDrawerForTheFirstTime(preferences: SharedPreferences, layout: DrawerLayout) {
+    val alreadySeen = preferences.getBoolean(DRAWER_LEARNED_KEY, false)
+    if (!alreadySeen) {
+        layout.openDrawer(Gravity.LEFT)
+        preferences.edit()
+                .putBoolean(DRAWER_LEARNED_KEY, true)
+                .apply()
+    }
+}
