9be2f93f404c4a9f020daeadfa259e48d3ffb923
SibWaf <dya-tel@yandex.ru>
2016-03-16 17:49:56 +0300

    Add SavedDataDAO to the MainActivity

--- app/src/main/java/ru/dyatel/tsuschedule/MainActivity.java
+++ app/src/main/java/ru/dyatel/tsuschedule/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import ru.dyatel.tsuschedule.data.DataFragment;
+import ru.dyatel.tsuschedule.data.SavedDataDAO;
 import ru.dyatel.tsuschedule.events.EventBus;
 import ru.dyatel.tsuschedule.fragments.MainFragment;
 import ru.dyatel.tsuschedule.layout.NavigationDrawerHandler;
@@ -16,6 +17,7 @@ import ru.dyatel.tsuschedule.layout.NavigationDrawerHandler;
 public class MainActivity extends AppCompatActivity {
 
     private EventBus eventBus;
+    private SavedDataDAO data;
 
     private NavigationHandler navigationHandler;
     private NavigationDrawerHandler drawerHandler;
@@ -26,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity);
 
         eventBus = new EventBus();
+        data = new SavedDataDAO(this);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
 
@@ -86,6 +89,10 @@ public class MainActivity extends AppCompatActivity {
 
     public EventBus getEventBus() {
         return eventBus;
+    }
+
+    public SavedDataDAO getData() {
+        return data;
     }
 
 }
