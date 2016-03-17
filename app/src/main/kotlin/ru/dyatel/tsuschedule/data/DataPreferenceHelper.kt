792c8658edba2196925a238ad1a58c37f79c3256
sibwaf <7131069+sibwaf@users.noreply.github.com>
2016-03-18 00:20:58 +0300

    Add DataPreferenceHelper for easy data access

---
+++ app/src/main/kotlin/ru/dyatel/tsuschedule/data/DataPreferenceHelper.kt
@@ -0,0 +1,26 @@
+package ru.dyatel.tsuschedule.data
+
+import android.content.Context
+
+private const val PREFERENCES = "data_preferences"
+private const val PREFERENCES_GROUP = "group"
+private const val PREFERENCES_SUBGROUP = "subgroup"
+
+private fun getPreferences(context: Context) =
+        context.getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE)
+
+fun getGroup(context: Context) =
+        getPreferences(context).getString(PREFERENCES_GROUP, "")
+
+fun setGroup(group: String, context: Context) =
+        getPreferences(context).edit()
+                .putString(PREFERENCES_GROUP, group)
+                .apply()
+
+fun getSubgroup(context: Context) =
+        getPreferences(context).getInt(PREFERENCES_SUBGROUP, 1)
+
+fun setSubgroup(subgroup: Int, context: Context) =
+        getPreferences(context).edit()
+                .putInt(PREFERENCES_SUBGROUP, subgroup)
+                .apply()
