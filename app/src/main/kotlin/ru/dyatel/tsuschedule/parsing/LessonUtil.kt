693919c34807eca105b722509e33e8510051f873
sibwaf <7131069+sibwaf@users.noreply.github.com>
2016-03-17 21:11:11 +0300

    Add utility to get a Lesson instance from the DB

--- app/src/main/kotlin/ru/dyatel/tsuschedule/parsing/LessonUtil.kt
+++ app/src/main/kotlin/ru/dyatel/tsuschedule/parsing/LessonUtil.kt
@@ -1,6 +1,7 @@
 package ru.dyatel.tsuschedule.parsing
 
 import android.content.ContentValues
+import android.database.Cursor
 import ru.dyatel.tsuschedule.data.LessonTable
 
 fun Lesson.toContentValues(): ContentValues {
@@ -15,3 +16,34 @@ fun Lesson.toContentValues(): ContentValues {
     values.put(LessonTable.SUBGROUP, subgroup)
     return values
 }
+
+private fun getColumnPairFromCursor(cursor: Cursor, column: String): Pair<String, Int> =
+        column to cursor.getColumnIndexOrThrow(column)
+
+fun getLessonColumnIndices(cursor: Cursor): Map<String, Int> =
+        mapOf(
+                getColumnPairFromCursor(cursor, LessonTable.PARITY),
+                getColumnPairFromCursor(cursor, LessonTable.WEEKDAY),
+                getColumnPairFromCursor(cursor, LessonTable.TIME),
+
+                getColumnPairFromCursor(cursor, LessonTable.DISCIPLINE),
+                getColumnPairFromCursor(cursor, LessonTable.AUDITORY),
+                getColumnPairFromCursor(cursor, LessonTable.TEACHER),
+
+                getColumnPairFromCursor(cursor, LessonTable.TYPE),
+                getColumnPairFromCursor(cursor, LessonTable.SUBGROUP)
+        )
+
+fun constructLessonFromCursor(cursor: Cursor, columnIndices: Map<String, Int>) =
+        Lesson(
+                Parity.valueOf(cursor.getString(columnIndices[LessonTable.PARITY]!!)),
+                cursor.getString(columnIndices[LessonTable.WEEKDAY]!!),
+                cursor.getString(columnIndices[LessonTable.TIME]!!),
+
+                cursor.getString(columnIndices[LessonTable.DISCIPLINE]!!),
+                cursor.getString(columnIndices[LessonTable.AUDITORY]!!),
+                cursor.getString(columnIndices[LessonTable.TEACHER]!!),
+
+                Lesson.Type.valueOf(cursor.getString(columnIndices[LessonTable.TYPE]!!)),
+                cursor.getInt(columnIndices[LessonTable.SUBGROUP]!!)
+        )
