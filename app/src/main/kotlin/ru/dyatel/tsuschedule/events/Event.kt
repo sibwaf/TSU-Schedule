73f5efdd442c2efec6fd0d010b5192480db5e66e
sibwaf <7131069+sibwaf@users.noreply.github.com>
2016-03-19 00:01:37 +0300

    Remove PREPARING_DATA_UPDATE event

--- app/src/main/kotlin/ru/dyatel/tsuschedule/events/Event.kt
+++ app/src/main/kotlin/ru/dyatel/tsuschedule/events/Event.kt
@@ -2,7 +2,6 @@ package ru.dyatel.tsuschedule.events
 
 enum class Event {
 
-    DATA_MODIFIER_SET_CHANGED,
-    PREPARING_DATA_UPDATE, DATA_UPDATED
+    DATA_MODIFIER_SET_CHANGED, DATA_UPDATED
 
 }
