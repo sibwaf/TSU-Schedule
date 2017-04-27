409350445972ce4fb76e93b06c6556af01028e8b
SibWaf <dya-tel@yandex.ru>
2017-04-27 16:26:31 +0300

    Refactor WeekFragment

--- app/src/main/kotlin/ru/dyatel/tsuschedule/fragments/WeekFragment.kt
+++ app/src/main/kotlin/ru/dyatel/tsuschedule/fragments/WeekFragment.kt
@@ -1,6 +1,5 @@
 package ru.dyatel.tsuschedule.fragments
 
-import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.widget.SwipeRefreshLayout
@@ -9,6 +8,9 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.find
+import org.jetbrains.anko.uiThread
 import ru.dyatel.tsuschedule.R
 import ru.dyatel.tsuschedule.ScheduleApplication
 import ru.dyatel.tsuschedule.data.LessonDao
@@ -22,30 +24,11 @@ import ru.dyatel.tsuschedule.parsing.Parity
 
 class WeekFragment(private val parity: Parity) : Fragment(), EventListener {
 
-    private inner class RefreshTask : AsyncTask<Void, Void, Void>() {
-
-        override fun onPreExecute() {
-            swipeRefresh?.isRefreshing = true
-        }
-
-        override fun doInBackground(vararg params: Void?): Void? {
-            val lessons = lessonDao!!.request(getSubgroup(context))
-            lessons.removeAll { it.parity != parity && it.parity != Parity.BOTH }
-            weekdays!!.updateData(lessons)
-            return null
-        }
-
-        override fun onPostExecute(result: Void?) {
-            swipeRefresh?.isRefreshing = false
-        }
-
-    }
-
     private var swipeRefresh: SwipeRefreshLayout? = null
-    private var weekdays: WeekdayListAdapter? = null
+    private lateinit var weekdays: WeekdayListAdapter
 
-    private var eventBus: EventBus? = null
-    private var lessonDao: LessonDao? = null
+    private lateinit var eventBus: EventBus
+    private lateinit var lessonDao: LessonDao
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,33 +39,44 @@ class WeekFragment(private val parity: Parity) : Fragment(), EventListener {
         eventBus = application.eventBus
         lessonDao = application.databaseManager.lessonDao
 
-        eventBus!!.subscribe(this, Event.DATA_UPDATED, Event.DATA_UPDATE_FAILED)
+        eventBus.subscribe(this, Event.DATA_UPDATED, Event.DATA_UPDATE_FAILED)
 
-        RefreshTask().execute()
+        refresh()
     }
 
     override fun onDestroy() {
-        eventBus!!.unsubscribe(this)
+        eventBus.unsubscribe(this)
         super.onDestroy()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = inflater.inflate(R.layout.week_fragment, container, false)
 
-        val weekdayList = view.findViewById(R.id.weekday_list) as RecyclerView
+        val weekdayList = view.find<RecyclerView>(R.id.weekday_list)
         weekdayList.layoutManager = LinearLayoutManager(context)
         weekdayList.adapter = weekdays
 
-        swipeRefresh = view.findViewById(R.id.swipe_refresh) as SwipeRefreshLayout
-        swipeRefresh!!.setOnRefreshListener { LessonFetchTask(context, eventBus!!, lessonDao!!).execute() }
+        swipeRefresh = view.find<SwipeRefreshLayout>(R.id.swipe_refresh)
+        swipeRefresh!!.setOnRefreshListener { LessonFetchTask(context, eventBus, lessonDao).execute() }
 
         return view
     }
 
     override fun handleEvent(type: Event) {
         when (type) {
-            Event.DATA_UPDATED -> activity.runOnUiThread { RefreshTask().execute() }
+            Event.DATA_UPDATED -> activity.runOnUiThread { refresh() }
             Event.DATA_UPDATE_FAILED -> activity.runOnUiThread { swipeRefresh?.isRefreshing = false }
+        }
+    }
+
+    private fun refresh() {
+        swipeRefresh?.isRefreshing = true
+        doAsync {
+            val lessons = lessonDao.request(getSubgroup(context))
+            lessons.removeAll { it.parity != parity && it.parity != Parity.BOTH }
+            weekdays.updateData(lessons)
+
+            uiThread { swipeRefresh?.isRefreshing = false }
         }
     }
 
