6be33b0ae70cd45fe9fe5fd4d4c271de6e31a2ee
sibwaf <7131069+sibwaf@users.noreply.github.com>
2017-04-28 17:58:30 +0300

    Move swipe refresh to MainFragment

--- app/src/main/kotlin/ru/dyatel/tsuschedule/fragments/MainFragment.kt
+++ app/src/main/kotlin/ru/dyatel/tsuschedule/fragments/MainFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.support.v4.view.ViewPager
+import android.support.v4.widget.SwipeRefreshLayout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,17 +12,40 @@ import hirondelle.date4j.DateTime
 import org.jetbrains.anko.find
 import ru.dyatel.tsuschedule.ParityReference
 import ru.dyatel.tsuschedule.R
+import ru.dyatel.tsuschedule.ScheduleApplication
+import ru.dyatel.tsuschedule.data.LessonFetchTask
+import ru.dyatel.tsuschedule.events.Event
+import ru.dyatel.tsuschedule.events.EventBus
+import ru.dyatel.tsuschedule.events.EventListener
 import ru.dyatel.tsuschedule.layout.WeekFragmentPagerAdapter
 import ru.dyatel.tsuschedule.parsing.DateUtil
 import java.util.TimeZone
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), EventListener {
+
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+
+    private lateinit var eventBus: EventBus
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val application = activity.application as ScheduleApplication
+        application.eventBus.subscribe(this, Event.DATA_UPDATE_FAILED, Event.DATA_UPDATED)
+    }
+
+    override fun onDestroy() {
+        eventBus.unsubscribe(this)
+        super.onDestroy()
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val root = inflater.inflate(R.layout.main_screen, container, false)
 
+        val adapter = WeekFragmentPagerAdapter(childFragmentManager)
+
         val pager = root.find<ViewPager>(R.id.pager)
-        pager.adapter = WeekFragmentPagerAdapter(childFragmentManager)
+        pager.adapter = adapter
         pager.currentItem = ParityReference.getIndexFromParity(
                 DateUtil.getWeekParity(DateTime.now(TimeZone.getDefault()))
         )
@@ -29,7 +53,17 @@ class MainFragment : Fragment() {
         val tabLayout = root.find<TabLayout>(R.id.tab_layout)
         tabLayout.setupWithViewPager(pager)
 
+        val lessonDao = (activity.application as ScheduleApplication).databaseManager.lessonDao
+        swipeRefresh = root.find<SwipeRefreshLayout>(R.id.swipe_refresh)
+        swipeRefresh.setOnRefreshListener { LessonFetchTask(context, eventBus, lessonDao).execute() }
+        swipeRefresh.setOnChildScrollUpCallback { _, _ ->
+            val week = adapter.getFragment(pager.currentItem)
+            week.view?.canScrollVertically(-1) ?: false
+        }
+
         return root
     }
+
+    override fun handleEvent(type: Event) = activity.runOnUiThread { swipeRefresh.isRefreshing = false }
 
 }
--- app/src/main/kotlin/ru/dyatel/tsuschedule/fragments/WeekFragment.kt
+++ app/src/main/kotlin/ru/dyatel/tsuschedule/fragments/WeekFragment.kt
@@ -2,7 +2,6 @@ package ru.dyatel.tsuschedule.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -10,11 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
-import org.jetbrains.anko.uiThread
 import ru.dyatel.tsuschedule.R
 import ru.dyatel.tsuschedule.ScheduleApplication
 import ru.dyatel.tsuschedule.data.LessonDao
-import ru.dyatel.tsuschedule.data.LessonFetchTask
 import ru.dyatel.tsuschedule.data.getSubgroup
 import ru.dyatel.tsuschedule.events.Event
 import ru.dyatel.tsuschedule.events.EventBus
@@ -24,7 +21,6 @@ import ru.dyatel.tsuschedule.parsing.Parity
 
 class WeekFragment(private val parity: Parity) : Fragment(), EventListener {
 
-    private var swipeRefresh: SwipeRefreshLayout? = null
     private lateinit var weekdays: WeekdayListAdapter
 
     private lateinit var eventBus: EventBus
@@ -39,7 +35,7 @@ class WeekFragment(private val parity: Parity) : Fragment(), EventListener {
         eventBus = application.eventBus
         lessonDao = application.databaseManager.lessonDao
 
-        eventBus.subscribe(this, Event.DATA_UPDATED, Event.DATA_UPDATE_FAILED)
+        eventBus.subscribe(this, Event.DATA_UPDATED)
 
         refresh()
     }
@@ -56,28 +52,17 @@ class WeekFragment(private val parity: Parity) : Fragment(), EventListener {
         weekdayList.layoutManager = LinearLayoutManager(context)
         weekdayList.adapter = weekdays
 
-        swipeRefresh = view.find<SwipeRefreshLayout>(R.id.swipe_refresh)
-        swipeRefresh!!.setOnRefreshListener { LessonFetchTask(context, eventBus, lessonDao).execute() }
-
         return view
     }
 
-    override fun handleEvent(type: Event) {
-        when (type) {
-            Event.DATA_UPDATED -> activity.runOnUiThread { refresh() }
-            Event.DATA_UPDATE_FAILED -> activity.runOnUiThread { swipeRefresh?.isRefreshing = false }
-        }
-    }
+    override fun handleEvent(type: Event) = refresh()
 
     private fun refresh() {
-        swipeRefresh?.isRefreshing = true
         doAsync {
             val lessons = lessonDao.request(getSubgroup(context))
                     .filter { it.parity == parity || it.parity == Parity.BOTH }
                     .toCollection(ArrayList())
             weekdays.updateData(lessons)
-
-            uiThread { swipeRefresh?.isRefreshing = false }
         }
     }
 
