1d29e02b2b2831fa15580c19cedd4aa18818b194
SibWaf <dya-tel@yandex.ru>
2017-04-30 12:33:20 +0300

    Migrate parsing exceptions to Kotlin

---
+++ src/main/kotlin/ru/dyatel/tsuschedule/parsing/ParsingExceptions.kt
@@ -0,0 +1,21 @@
+package ru.dyatel.tsuschedule.parsing
+
+import java.lang.IllegalArgumentException
+
+class ParsingException : RuntimeException {
+
+    constructor() : super()
+    constructor(message: String) : super(message)
+    constructor(message: String, cause: Throwable) : super(message, cause)
+    constructor(cause: Throwable) : super(cause)
+
+}
+
+class BadGroupException : IllegalArgumentException {
+
+    constructor() : super()
+    constructor(message: String) : super(message)
+    constructor(message: String, cause: Throwable) : super(message, cause)
+    constructor(cause: Throwable) : super(cause)
+
+}
