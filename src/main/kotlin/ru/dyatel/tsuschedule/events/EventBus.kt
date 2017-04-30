fad5d5cb5ed77d59d4030b169c020df2298d2647
sibwaf <7131069+sibwaf@users.noreply.github.com>
2017-04-30 20:11:30 +0300

    Make EventBus a little bit prettier

--- src/main/kotlin/ru/dyatel/tsuschedule/events/EventBus.kt
+++ src/main/kotlin/ru/dyatel/tsuschedule/events/EventBus.kt
@@ -3,13 +3,18 @@ package ru.dyatel.tsuschedule.events
 import java.util.ArrayList
 import java.util.EnumMap
 
+private typealias EventListenerList = MutableList<EventListener>
+
 object EventBus {
 
-    private val listeners = EnumMap<Event, MutableList<EventListener>>(Event::class.java)
+    private val listeners = EnumMap<Event, EventListenerList>(Event::class.java)
 
     fun subscribe(listener: EventListener, vararg types: Event) {
         val typeArray = if (types.isEmpty()) Event.values() else types
-        typeArray.forEach { listeners.getOrPut(it, { ArrayList() }) += listener }
+        typeArray.forEach {
+            val list = listeners.getOrPut(it, { ArrayList() })
+            list += listener
+        }
     }
 
     fun unsubscribe(listener: EventListener) = listeners.values.forEach { it.remove(listener) }
