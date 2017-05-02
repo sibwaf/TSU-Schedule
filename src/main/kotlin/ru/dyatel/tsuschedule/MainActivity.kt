8605205ce73406ca30106282446f5993e0eb1059
sibwaf <7131069+sibwaf@users.noreply.github.com>
2017-05-02 21:32:16 +0300

    Replace navigation drawer with Material Drawer

--- src/main/kotlin/ru/dyatel/tsuschedule/MainActivity.kt
+++ src/main/kotlin/ru/dyatel/tsuschedule/MainActivity.kt
@@ -1,56 +1,46 @@
 package ru.dyatel.tsuschedule
 
-import android.content.res.Configuration
 import android.os.Bundle
-import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.MenuItem
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
 import org.jetbrains.anko.find
 import ru.dyatel.tsuschedule.fragments.MainFragment
-import ru.dyatel.tsuschedule.layout.NavigationDrawerHandler
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var drawerHandler: NavigationDrawerHandler
+    private lateinit var drawer: Drawer
+
     private lateinit var navigationHandler: NavigationHandler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity)
 
-        setSupportActionBar(find<Toolbar>(R.id.toolbar))
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setHomeButtonEnabled(true)
+        val toolbar = find<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        val drawer = find<DrawerLayout>(R.id.drawer_layout)
-        drawerHandler = NavigationDrawerHandler(this, drawer)
+        val drawerHeader = layoutInflater.inflate(R.layout.navigation_drawer, null, false)
 
-        navigationHandler = NavigationHandler(fragmentManager, drawerHandler)
-        fragmentManager.addOnBackStackChangedListener(navigationHandler)
+        drawer = DrawerBuilder()
+                .withActivity(this)
+                .withRootView(R.id.drawer_layout)
+                .withToolbar(toolbar)
+                .withStickyHeader(drawerHeader)
+                .withTranslucentStatusBar(false)
+                .withActionBarDrawerToggleAnimated(true)
+                .build()
+
+        navigationHandler = NavigationHandler(fragmentManager, drawer, supportActionBar)
 
         fragmentManager.beginTransaction()
                 .replace(R.id.content_fragment, MainFragment())
                 .commit()
     }
 
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
-        drawerHandler.syncState()
-    }
-
     override fun onBackPressed() {
         if (!navigationHandler.onBackPressed()) super.onBackPressed()
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return navigationHandler.onOptionsItemSelected(item) ||
-                super.onOptionsItemSelected(item)
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        drawerHandler.onConfigurationChanged(newConfig)
     }
 
 }
