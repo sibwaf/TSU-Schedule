205863cf4ec5ae31b2bcccaee1a1545268150819
SibWaf <dya-tel@yandex.ru>
2017-08-25 20:25:52 +0300

    Make exception handling in LessonFetcher prettier

--- app/src/main/kotlin/ru/dyatel/tsuschedule/data/LessonFetcher.kt
+++ app/src/main/kotlin/ru/dyatel/tsuschedule/data/LessonFetcher.kt
@@ -22,8 +22,7 @@ fun asyncLessonFetch(context: Context, data: LessonDao) = with(context) {
 
     val group = preferences.group
     if (group.isBlank()) {
-        EventBus.broadcast(Event.DATA_UPDATE_FAILED)
-        longToast(R.string.failure_missing_group_index)
+        fail(R.string.failure_missing_group_index)
         return@with
     }
 
@@ -31,26 +30,29 @@ fun asyncLessonFetch(context: Context, data: LessonDao) = with(context) {
         val parser = Parser()
         parser.setTimeout(preferences.connectionTimeout * 1000)
 
-        try {
+        val failure = try {
             data.update(parser.getLessons(group))
+            null
+        } catch (e: BadGroupException) {
+            R.string.failure_wrong_group_index
+        } catch (e: ParsingException) {
+            if (BuildConfig.DEBUG) Log.e("LessonFetcher", "Failed to parse the response", e)
+            else Crashlytics.logException(e)
+            R.string.failure_parsing_failed
+        } catch (e: SocketTimeoutException) {
+            R.string.failure_connection_timeout
+        } catch (e: IOException) {
+            R.string.failure_unsuccessful_request
         } catch (e: Exception) {
-            EventBus.broadcast(Event.DATA_UPDATE_FAILED)
-            val failureTextRes = when (e) {
-                is BadGroupException -> R.string.failure_wrong_group_index
-                is ParsingException -> {
-                    if (BuildConfig.DEBUG) Log.e("LessonFetcher", "Failed to parse the response", e)
-                    else Crashlytics.logException(e)
-                    R.string.failure_parsing_failed
-                }
-                is SocketTimeoutException -> R.string.failure_connection_timeout
-                is IOException -> R.string.failure_unsuccessful_request
-                else -> {
-                    uiThread { throw e }
-                    return@doAsync
-                }
-            }
-
-            uiThread { longToast(failureTextRes) }
+            uiThread { throw e }
+            return@doAsync
         }
+
+        failure?.let { uiThread { fail(failure) } }
     }
+}
+
+private fun Context.fail(textResource: Int) {
+    EventBus.broadcast(Event.DATA_UPDATE_FAILED)
+    longToast(textResource)
 }
