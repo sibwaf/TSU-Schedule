6e7f6cac85cdb5ad36a827faae8a09f8ccf23344
sibwaf <7131069+sibwaf@users.noreply.github.com>
2017-08-29 12:37:37 +0300

    Merge MainFragment and WeekFragment

--- app/src/main/kotlin/ru/dyatel/tsuschedule/fragments/MainFragment.kt
+++ app/src/main/kotlin/ru/dyatel/tsuschedule/fragments/MainFragment.kt
@@ -9,29 +9,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.jetbrains.anko.ctx
+import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
+import org.jetbrains.anko.runOnUiThread
 import ru.dyatel.tsuschedule.R
 import ru.dyatel.tsuschedule.ScheduleApplication
+import ru.dyatel.tsuschedule.data.Lesson
+import ru.dyatel.tsuschedule.data.LessonDao
+import ru.dyatel.tsuschedule.data.Parity
 import ru.dyatel.tsuschedule.data.asyncLessonFetch
+import ru.dyatel.tsuschedule.data.currentWeekParity
 import ru.dyatel.tsuschedule.events.Event
 import ru.dyatel.tsuschedule.events.EventBus
 import ru.dyatel.tsuschedule.events.EventListener
-import ru.dyatel.tsuschedule.layout.WeekFragmentPagerAdapter
-import ru.dyatel.tsuschedule.data.currentWeekParity
+import ru.dyatel.tsuschedule.layout.WeekDataContainer
+import ru.dyatel.tsuschedule.layout.WeekPagerAdapter
+import ru.dyatel.tsuschedule.schedulePreferences
 
 class MainFragment : Fragment(), EventListener {
 
-    private lateinit var weekAdapter: WeekFragmentPagerAdapter
     private lateinit var swipeRefresh: SwipeRefreshLayout
-
     private var blockSwipeRefresh = false
+
+    private lateinit var lessons: LessonDao
+
+    private val weeks = WeekDataContainer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        weekAdapter = WeekFragmentPagerAdapter(childFragmentManager, ctx)
+        lessons = (activity.application as ScheduleApplication).databaseManager.lessonDao
 
         EventBus.subscribe(this, Event.DATA_UPDATE_FAILED, Event.DATA_UPDATED)
+        doAsync { handleEvent(Event.DATA_UPDATED, null) }
     }
 
     override fun onDestroy() {
@@ -39,26 +49,47 @@ class MainFragment : Fragment(), EventListener {
         super.onDestroy()
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle?): View {
         val root = inflater.inflate(R.layout.main_screen, container, false)
 
-        val pager = root.find<ViewPager>(R.id.pager)
-        pager.adapter = weekAdapter
-        pager.currentItem = currentWeekParity.index
-        pager.addOnPageChangeListener(pagerScrollListener)
+        val pager = root.find<ViewPager>(R.id.pager).apply {
+            val _adapter = WeekPagerAdapter(ctx, weeks)
+            adapter = _adapter
+            currentItem = _adapter.getPosition(currentWeekParity)
+            addOnPageChangeListener(pagerScrollListener)
+        }
 
         val tabLayout = root.find<TabLayout>(R.id.tab_layout)
         tabLayout.setupWithViewPager(pager)
 
         val lessonDao = (activity.application as ScheduleApplication).databaseManager.lessonDao
-        swipeRefresh = root.find<SwipeRefreshLayout>(R.id.swipe_refresh)
+        swipeRefresh = root.find(R.id.swipe_refresh)
         swipeRefresh.setOnRefreshListener { asyncLessonFetch(ctx, lessonDao) }
         swipeRefresh.setOnChildScrollUpCallback { _, _ -> blockSwipeRefresh }
 
         return root
     }
 
-    override fun handleEvent(type: Event, payload: Any?) = activity.runOnUiThread { swipeRefresh.isRefreshing = false }
+    override fun handleEvent(type: Event, payload: Any?) {
+        if (type == Event.DATA_UPDATED) {
+            val odd = mutableListOf<Lesson>()
+            val even = mutableListOf<Lesson>()
+            lessons.request(ctx.schedulePreferences.subgroup).forEach {
+                when (it.parity) {
+                    Parity.BOTH -> {
+                        odd += it
+                        even += it
+                    }
+                    Parity.ODD -> odd += it
+                    Parity.EVEN -> even += it
+                }
+            }
+            runOnUiThread {
+                weeks.updateData(odd, even)
+                swipeRefresh.isRefreshing = false
+            }
+        } else runOnUiThread { swipeRefresh.isRefreshing = false }
+    }
 
     private val pagerScrollListener = object : ViewPager.OnPageChangeListener {
 
--- app/src/main/kotlin/ru/dyatel/tsuschedule/layout/WeekdayListAdapter.kt
+++ app/src/main/kotlin/ru/dyatel/tsuschedule/layout/WeekdayListAdapter.kt
@@ -1,6 +1,5 @@
 package ru.dyatel.tsuschedule.layout
 
-import android.app.Activity
 import android.support.v4.view.ViewCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -17,48 +16,52 @@ private val NORMAL_WEEKDAY_ORDER = arrayOf(
         "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"
 )
 
-class WeekdayListAdapter(private val activity: Activity) : RecyclerView.Adapter<WeekdayListAdapter.Holder>() {
+private typealias Weekday = Pair<String, List<Lesson>>
 
-    class Holder(v: View, activity: Activity) : RecyclerView.ViewHolder(v) {
+class WeekdayListAdapter : RecyclerView.Adapter<WeekdayListAdapter.Holder>() {
+
+    class Holder(v: View) : RecyclerView.ViewHolder(v) {
 
         val weekdayName = v.find<TextView>(R.id.weekday)
-        val lessonList = v.find<RecyclerView>(R.id.lesson_list)
-
-        val adapter = LessonListAdapter(activity)
+        private val lessonListAdapter = LessonListAdapter()
 
         init {
-            ViewCompat.setNestedScrollingEnabled(lessonList, false)
-            lessonList.layoutManager = LinearLayoutManager(activity)
-            lessonList.adapter = adapter
+            v.find<RecyclerView>(R.id.lesson_list).apply {
+                ViewCompat.setNestedScrollingEnabled(this, false)
+                layoutManager = LinearLayoutManager(v.context)
+                adapter = lessonListAdapter
+            }
         }
+
+        fun updateData(lessons: List<Lesson>) = lessonListAdapter.updateData(lessons)
 
     }
 
-    private val weekdays = ArrayList<Pair<String, List<Lesson>>>()
+    private val weekdays = ArrayList<Weekday>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.weekday, parent, false)
-        return Holder(view, activity)
+        return Holder(view)
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         val (weekday, lessons) = weekdays[position]
         holder.weekdayName.text = weekday
-        holder.adapter.updateData(lessons)
+        holder.updateData(lessons)
     }
 
-    override fun getItemCount(): Int = weekdays.size
+    override fun getItemCount() = weekdays.size
 
     fun updateData(lessons: List<Lesson>) {
         weekdays.clear()
 
         NORMAL_WEEKDAY_ORDER.forEach { weekday ->
-            val weekdayLessons = lessons.filter { it.weekday.toLowerCase() == weekday }
-                    .toCollection(ArrayList<Lesson>())
-            if (weekdayLessons.size > 0) weekdays += weekday to weekdayLessons
+            lessons.filter { it.weekday.toLowerCase() == weekday }
+                    .takeIf { it.isNotEmpty() }
+                    ?.let { weekdays += weekday to it.toList() }
         }
 
-        activity.runOnUiThread { notifyDataSetChanged() }
+        notifyDataSetChanged()
     }
 
 }
