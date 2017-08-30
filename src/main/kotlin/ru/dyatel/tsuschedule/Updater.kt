913e1a1ae24b88567e427c45c7e11a9ad84faf66
sibwaf <7131069+sibwaf@users.noreply.github.com>
2017-08-31 01:11:44 +0300

    Implement proper release comparison

--- src/main/kotlin/ru/dyatel/tsuschedule/Updater.kt
+++ src/main/kotlin/ru/dyatel/tsuschedule/Updater.kt
@@ -22,7 +22,42 @@ private const val URL = "https://api.github.com/repos/$REPOSITORY/releases/lates
 private const val MIME_APK = "application/vnd.android.package-archive"
 
 data class Release(val version: String, val url: String) {
-    fun isNewerThanInstalled() = version != BuildConfig.VERSION_NAME
+
+    private companion object {
+        val VERSION_PATTERN = Regex("^((?:\\d+)(?:\\.\\d+)*)(.*)$")
+    }
+
+    fun isNewerThanInstalled(): Boolean {
+        val match = VERSION_PATTERN.matchEntire(BuildConfig.VERSION_NAME)
+                ?: throw RuntimeException("Current version name is malformed")
+
+        val number = match.groupValues[1]
+        val isPrerelease = match.groupValues[2].isNotEmpty()
+
+        val currentVersionComponents = number.split(".")
+        val releaseVersionComponents = version.split(".")
+
+        val componentCount = minOf(currentVersionComponents.size, releaseVersionComponents.size)
+
+        for (i in 0 until componentCount) {
+            if (releaseVersionComponents[i] > currentVersionComponents[i]) return true
+            if (releaseVersionComponents[i] < currentVersionComponents[i]) return false
+        }
+
+        if (currentVersionComponents.size > componentCount) {
+            val sum = currentVersionComponents
+                    .filterIndexed { index, _ -> index >= componentCount }
+                    .sumBy { it.toInt() }
+            if (sum > 0) return false
+        } else if (releaseVersionComponents.size > componentCount) {
+            val sum = releaseVersionComponents
+                    .filterIndexed { index, _ -> index >= componentCount }
+                    .sumBy { it.toInt() }
+            if (sum > 0) return true
+        }
+
+        return isPrerelease
+    }
 }
 
 class Updater {
