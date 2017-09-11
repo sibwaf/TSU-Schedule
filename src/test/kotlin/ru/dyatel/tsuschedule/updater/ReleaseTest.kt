a24b402a83601b793be5f8ceb7ae29fa30c674c8
SibWaf <dya-tel@yandex.ru>
2017-09-11 22:03:56 +0300

    Make Release comparable, allow alpha versions

---
+++ src/test/kotlin/ru/dyatel/tsuschedule/updater/ReleaseTest.kt
@@ -0,0 +1,33 @@
+package ru.dyatel.tsuschedule.updater
+
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class ReleaseTest {
+
+    private fun release(version: String) = Release(version, "")
+
+    @Test fun testBuildVersionName() {
+        Release.CURRENT
+    }
+
+    @Test fun testEqualRelease() {
+        assertEquals(0, release("0.1.0").compareTo(release("0.1.0.0.0")))
+        assertEquals(0, release("1.0.0").compareTo(release("1.0.0.0.0")))
+        assertEquals(0, release("1.0.0-alpha1").compareTo(release("1.0.0.0.0-alpha1")))
+    }
+
+    @Test fun testLesserRelease() {
+        assertTrue(release("0.1.0.0.0") < release("0.2.0"))
+        assertTrue(release("0.1.0") < release("0.1.0.0.0.1"))
+        assertTrue(release("1.0.0-alpha1") < release("1.0.0"))
+    }
+
+    @Test fun testGreaterRelease() {
+        assertTrue(release("0.2.0") > release("0.1.0.0.0"))
+        assertTrue(release("0.1.0.0.0.1") > release("0.1.0"))
+        assertTrue(release("1.0.0") > release("1.0.0-alpha1"))
+    }
+
+}
