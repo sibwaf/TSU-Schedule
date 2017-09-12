457bf56112deac15e681f03cc6e8cbab1604070b
sibwaf <7131069+sibwaf@users.noreply.github.com>
2017-09-12 17:31:26 +0300

    Add Iterator extension for JSONArray

--- src/main/kotlin/ru/dyatel/tsuschedule/utilities/JsonUtilities.kt
+++ src/main/kotlin/ru/dyatel/tsuschedule/utilities/JsonUtilities.kt
@@ -1,8 +1,19 @@
 package ru.dyatel.tsuschedule.utilities
 
+import org.json.JSONArray
 import org.json.JSONObject
 
 inline fun <reified T> JSONObject.find(name: String): T {
     val result = get(name) ?: throw NoSuchElementException()
     return result as T
+}
+
+operator fun JSONArray.iterator() = object : Iterator<Any> {
+
+    private var current = 0
+
+    override fun hasNext() = current < this@iterator.length()
+
+    override fun next() = this@iterator[current++]
+
 }
