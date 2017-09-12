96a84fd689dfb5da86db4fa7d4dcf496a5911225
sibwaf <7131069+sibwaf@users.noreply.github.com>
2017-09-12 15:55:21 +0300

    Better exception handling in UpdaterApi parser

--- src/main/kotlin/ru/dyatel/tsuschedule/updater/UpdaterApi.kt
+++ src/main/kotlin/ru/dyatel/tsuschedule/updater/UpdaterApi.kt
@@ -31,28 +31,33 @@ class UpdaterApi {
         if (response.statusCode() == HttpURLConnection.HTTP_NOT_FOUND)
             return null
 
-        val obj = JSONObject(response.body())
+        try {
+            val obj = JSONObject(response.body())
 
-        val rawVersion = obj.find<String>("tag_name")
-        val version = VERSION_PATTERN.matchEntire(rawVersion)?.groupValues?.get(1)
-                ?: throw ParsingException("Version tag is malformed: <$rawVersion>")
+            val rawVersion = obj.find<String>("tag_name")
+            val version = VERSION_PATTERN.matchEntire(rawVersion)?.groupValues?.get(1)
+                    ?: throw ParsingException("Version tag is malformed: <$rawVersion>")
 
-        val links = mutableListOf<String>()
+            val links = mutableListOf<String>()
 
-        val assets = obj.find<JSONArray>("assets")
-        for (i in 0 until assets.length()) {
-            val asset = assets[i] as JSONObject
+            val assets = obj.find<JSONArray>("assets")
+            for (i in 0 until assets.length()) {
+                val asset = assets[i] as JSONObject
 
-            val link = asset.find<String>("browser_download_url")
-            val mime = asset.find<String>("content_type")
+                val link = asset.find<String>("browser_download_url")
+                val mime = asset.find<String>("content_type")
 
-            if (mime == MIME_APK) links += link
+                if (mime == MIME_APK) links += link
+            }
+
+            if (links.isEmpty()) throw ParsingException("Didn't find an .apk in assets")
+            val url = links.singleOrNull() ?: throw ParsingException("Too many .apk files in assets")
+
+            return Release(version, url)
+        } catch (e: Exception) {
+            if (e is ParsingException) throw e
+            else throw ParsingException(e)
         }
-
-        if (links.isEmpty()) throw ParsingException("Didn't find an .apk in assets")
-        val url = links.singleOrNull() ?: throw ParsingException("Too many .apk files in assets")
-
-        return Release(version, url)
     }
 
 }
