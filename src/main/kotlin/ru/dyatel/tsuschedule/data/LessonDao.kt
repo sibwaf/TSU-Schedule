06cd22828bc30ad9e75ab42efdabe1127bc527af
sibwaf <7131069+sibwaf@users.noreply.github.com>
2017-09-22 23:43:29 +0300

    Refactor LessonDao

--- src/main/kotlin/ru/dyatel/tsuschedule/data/LessonDao.kt
+++ src/main/kotlin/ru/dyatel/tsuschedule/data/LessonDao.kt
@@ -14,25 +14,55 @@ import ru.dyatel.tsuschedule.events.Event
 import ru.dyatel.tsuschedule.events.EventBus
 import ru.dyatel.tsuschedule.events.EventListener
 
-private const val TABLE_UNFILTERED = "lessons"
-private const val TABLE_FILTERED = "filtered"
-
-private object LessonColumns {
-
-    const val PARITY = "parity"
-    const val WEEKDAY = "weekday"
-    const val TIME = "time"
-
-    const val DISCIPLINE = "discipline"
-    const val AUDITORY = "auditory"
-    const val TEACHER = "teacher"
-
-    const val TYPE = "type"
-    const val SUBGROUP = "subgroup"
-
-}
-
 class LessonDao(databaseManager: DatabaseManager) : DatabasePart(databaseManager), EventListener {
+
+    private object Columns {
+        const val PARITY = "parity"
+        const val WEEKDAY = "weekday"
+        const val TIME = "time"
+
+        const val DISCIPLINE = "discipline"
+        const val AUDITORY = "auditory"
+        const val TEACHER = "teacher"
+
+        const val TYPE = "type"
+        const val SUBGROUP = "subgroup"
+    }
+
+    private companion object {
+
+        const val TABLE_UNFILTERED = "lessons"
+        const val TABLE_FILTERED = "filtered"
+
+        val LESSON_PARSER = object : MapRowParser<Lesson> {
+            override fun parseRow(columns: Map<String, Any?>): Lesson {
+                return Lesson(
+                        Parity.valueOf(columns[Columns.PARITY] as String),
+                        columns[Columns.WEEKDAY] as String,
+                        columns[Columns.TIME] as String,
+                        columns[Columns.DISCIPLINE] as String,
+                        columns[Columns.AUDITORY] as String?,
+                        columns[Columns.TEACHER] as String?,
+                        LessonType.valueOf(columns[Columns.TYPE] as String),
+                        (columns[Columns.SUBGROUP] as Long?)?.toInt()
+                )
+            }
+        }
+
+    }
+
+    private fun Lesson.toContentValues(): ContentValues {
+        val values = ContentValues()
+        values.put(Columns.PARITY, parity.toString())
+        values.put(Columns.WEEKDAY, weekday)
+        values.put(Columns.TIME, time)
+        values.put(Columns.DISCIPLINE, discipline)
+        values.put(Columns.AUDITORY, auditory)
+        values.put(Columns.TEACHER, teacher)
+        values.put(Columns.TYPE, type.toString())
+        values.put(Columns.SUBGROUP, subgroup)
+        return values
+    }
 
     init {
         EventBus.subscribe(this, Event.DATA_MODIFIER_SET_CHANGED)
@@ -41,14 +71,14 @@ class LessonDao(databaseManager: DatabaseManager) : DatabasePart(databaseManager
     override fun createTables(db: SQLiteDatabase) {
         listOf(TABLE_UNFILTERED, TABLE_FILTERED).forEach {
             db.createTable(it, true,
-                    LessonColumns.PARITY to TEXT,
-                    LessonColumns.WEEKDAY to TEXT,
-                    LessonColumns.TIME to TEXT,
-                    LessonColumns.DISCIPLINE to TEXT,
-                    LessonColumns.AUDITORY to TEXT,
-                    LessonColumns.TEACHER to TEXT,
-                    LessonColumns.TYPE to TEXT,
-                    LessonColumns.SUBGROUP to INTEGER
+                    Columns.PARITY to TEXT,
+                    Columns.WEEKDAY to TEXT,
+                    Columns.TIME to TEXT,
+                    Columns.DISCIPLINE to TEXT,
+                    Columns.AUDITORY to TEXT,
+                    Columns.TEACHER to TEXT,
+                    Columns.TYPE to TEXT,
+                    Columns.SUBGROUP to INTEGER
             )
         }
     }
@@ -79,7 +109,7 @@ class LessonDao(databaseManager: DatabaseManager) : DatabasePart(databaseManager
         writableDatabase.transaction {
             delete(TABLE_FILTERED)
 
-            select(TABLE_UNFILTERED).parseList(lessonParser)
+            select(TABLE_UNFILTERED).parseList(LESSON_PARSER)
                     .mapNotNull {
                         var result: Lesson? = it
                         for (filter in filters) {
@@ -97,39 +127,9 @@ class LessonDao(databaseManager: DatabaseManager) : DatabasePart(databaseManager
 
     fun getLessons(): List<Lesson> = readableDatabase
             .select(TABLE_FILTERED)
-            .orderBy(LessonColumns.TIME)
-            .parseList(lessonParser)
+            .orderBy(Columns.TIME)
+            .parseList(LESSON_PARSER)
 
     override fun handleEvent(type: Event, payload: Any?) = applyModifiers()
-
-}
-
-private fun Lesson.toContentValues(): ContentValues {
-    val values = ContentValues()
-    values.put(LessonColumns.PARITY, parity.toString())
-    values.put(LessonColumns.WEEKDAY, weekday)
-    values.put(LessonColumns.TIME, time)
-    values.put(LessonColumns.DISCIPLINE, discipline)
-    values.put(LessonColumns.AUDITORY, auditory)
-    values.put(LessonColumns.TEACHER, teacher)
-    values.put(LessonColumns.TYPE, type.toString())
-    values.put(LessonColumns.SUBGROUP, subgroup)
-    return values
-}
-
-private val lessonParser = object : MapRowParser<Lesson> {
-
-    override fun parseRow(columns: Map<String, Any?>): Lesson {
-        return Lesson(
-                Parity.valueOf(columns[LessonColumns.PARITY] as String),
-                columns[LessonColumns.WEEKDAY] as String,
-                columns[LessonColumns.TIME] as String,
-                columns[LessonColumns.DISCIPLINE] as String,
-                columns[LessonColumns.AUDITORY] as String?,
-                columns[LessonColumns.TEACHER] as String?,
-                LessonType.valueOf(columns[LessonColumns.TYPE] as String),
-                (columns[LessonColumns.SUBGROUP] as Long?)?.toInt()
-        )
-    }
 
 }
