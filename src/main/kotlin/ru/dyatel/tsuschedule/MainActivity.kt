f17425e29b727736f0e2632f09b1dc631706f753
sibwaf <7131069+sibwaf@users.noreply.github.com>
2018-01-11 20:04:55 +0300

    Update libraries and Gradle

--- src/main/kotlin/ru/dyatel/tsuschedule/MainActivity.kt
+++ src/main/kotlin/ru/dyatel/tsuschedule/MainActivity.kt
@@ -3,9 +3,9 @@ package ru.dyatel.tsuschedule
 import android.app.PendingIntent
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.NotificationCompat
 import android.support.v4.view.ViewCompat
 import android.support.v4.widget.DrawerLayout
-import android.support.v7.app.NotificationCompat
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
