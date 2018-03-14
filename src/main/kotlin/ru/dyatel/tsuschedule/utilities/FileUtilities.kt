7574d6dd577fe907bdcb9c8d99329bd69e59cc33
sibwaf <7131069+sibwaf@users.noreply.github.com>
2018-03-14 20:22:31 +0300

    Prettify FileUtilities

--- src/main/kotlin/ru/dyatel/tsuschedule/utilities/FileUtilities.kt
+++ src/main/kotlin/ru/dyatel/tsuschedule/utilities/FileUtilities.kt
@@ -3,23 +3,23 @@ package ru.dyatel.tsuschedule.utilities
 import java.io.File
 import java.net.URL
 
+private const val BUFFER_SIZE = 8192
+
 fun URL.download(destination: File, timeout: Int, onProgressUpdate: (Int) -> Unit) {
     val connection = openConnection().apply {
         connectTimeout = timeout
         readTimeout = timeout
     }
 
-    connection.getInputStream().buffered().use { input ->
-        destination.outputStream().buffered().use { output ->
-            val buffer = ByteArray(8192)
+    connection.getInputStream().buffered(BUFFER_SIZE).use { input ->
+        destination.outputStream().buffered(BUFFER_SIZE).use { output ->
+            val buffer = ByteArray(BUFFER_SIZE)
+
             val length = connection.contentLength
             var downloaded = 0
-            var count: Int
-            do {
-                count = input.read(buffer)
-                if (count == -1)
-                    break
 
+            do {
+                val count = input.read(buffer).takeUnless { it == -1 } ?: break
                 output.write(buffer, 0, count)
 
                 if (length > 0) {
