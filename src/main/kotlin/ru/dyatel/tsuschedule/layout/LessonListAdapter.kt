d009f0fec28359f29d781641b52d8470c3f1e6ee
SibWaf <dya-tel@yandex.ru>
2018-03-17 01:29:04 +0300

    Extract lesson views to separate classes

--- src/main/kotlin/ru/dyatel/tsuschedule/layout/LessonListAdapter.kt
+++ src/main/kotlin/ru/dyatel/tsuschedule/layout/LessonListAdapter.kt
@@ -1,112 +1,25 @@
 package ru.dyatel.tsuschedule.layout
 
 import android.support.v7.widget.RecyclerView
-import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
-import org.jetbrains.anko.alignParentLeft
-import org.jetbrains.anko.alignParentRight
-import org.jetbrains.anko.backgroundResource
-import org.jetbrains.anko.dip
-import org.jetbrains.anko.find
-import org.jetbrains.anko.leftOf
-import org.jetbrains.anko.linearLayout
-import org.jetbrains.anko.margin
-import org.jetbrains.anko.matchParent
-import org.jetbrains.anko.relativeLayout
-import org.jetbrains.anko.textView
-import org.jetbrains.anko.verticalLayout
-import org.jetbrains.anko.view
-import ru.dyatel.tsuschedule.R
 import ru.dyatel.tsuschedule.data.Lesson
-import ru.dyatel.tsuschedule.data.LessonType
-import ru.dyatel.tsuschedule.utilities.hideIf
 import java.util.ArrayList
 
 class LessonListAdapter : RecyclerView.Adapter<LessonListAdapter.Holder>() {
 
-    private companion object {
-        val typeMarkerViewId = View.generateViewId()
-        val timeViewId = View.generateViewId()
-        val auditoryViewId = View.generateViewId()
-        val disciplineViewId = View.generateViewId()
-        val teacherViewId = View.generateViewId()
-    }
-
-    class Holder(v: View) : RecyclerView.ViewHolder(v) {
-        val typeMarker = v.find<View>(typeMarkerViewId)
-        val time = v.find<TextView>(timeViewId)
-        val auditory = v.find<TextView>(auditoryViewId)
-        val discipline = v.find<TextView>(disciplineViewId)
-        val teacher = v.find<TextView>(teacherViewId)
-    }
+    class Holder(val view: GroupLessonView) : RecyclerView.ViewHolder(view)
 
     private val lessons = ArrayList<Lesson>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val view = parent.context.linearLayout {
-            lparams(width = matchParent) {
-                orientation = LinearLayout.HORIZONTAL
-                margin = dip(2)
-            }
-
-            view { id = typeMarkerViewId }.lparams(width = dip(4), height = matchParent) {
-                rightMargin = dip(4)
-            }
-
-            verticalLayout {
-                lparams(width = matchParent)
-
-                relativeLayout {
-                    lparams(width = matchParent)
-
-                    textView { id = timeViewId }.lparams {
-                        leftOf(auditoryViewId)
-                        alignParentLeft()
-                    }
-                    textView { id = auditoryViewId }.lparams {
-                        alignParentRight()
-                    }
-                }
-
-                textView { id = disciplineViewId }
-                textView { id = teacherViewId }
-            }
-        }
-
-        return Holder(view)
+        return Holder(GroupLessonView(parent.context))
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        val lesson = lessons[position]
-        with(holder) {
-            val context = itemView.context
-
-            val typeText = when (lesson.type) {
-                LessonType.PRACTICE -> R.string.lesson_type_practice
-                LessonType.LECTURE -> R.string.lesson_type_lecture
-                LessonType.LABORATORY -> R.string.lesson_type_laboratory
-                LessonType.UNKNOWN -> null
-            }?.let { context.getString(it) }
-
-            typeMarker.backgroundResource = when (lesson.type) {
-                LessonType.PRACTICE -> R.color.practice_color
-                LessonType.LECTURE -> R.color.lecture_color
-                LessonType.LABORATORY -> R.color.laboratory_color
-                LessonType.UNKNOWN -> R.color.unknown_color
-            }
-            time.text = lesson.time
-            auditory.text = lesson.auditory
-            discipline.text = typeText?.let { "${lesson.discipline} ($typeText)" } ?: lesson.discipline
-            teacher.text = lesson.teacher
-
-            auditory.hideIf { lesson.auditory == null }
-            teacher.hideIf { lesson.teacher == null }
-        }
+        holder.view.bind(lessons[position])
     }
 
-    override fun getItemCount(): Int = lessons.size
+    override fun getItemCount() = lessons.size
 
     fun updateData(lessons: List<Lesson>) {
         with(this.lessons) {
---
+++ src/main/kotlin/ru/dyatel/tsuschedule/layout/LessonView.kt
@@ -0,0 +1,155 @@
+package ru.dyatel.tsuschedule.layout
+
+import android.content.Context
+import android.view.View
+import android.widget.TextView
+import org.jetbrains.anko._LinearLayout
+import org.jetbrains.anko.alignParentLeft
+import org.jetbrains.anko.alignParentRight
+import org.jetbrains.anko.backgroundResource
+import org.jetbrains.anko.dip
+import org.jetbrains.anko.find
+import org.jetbrains.anko.leftOf
+import org.jetbrains.anko.margin
+import org.jetbrains.anko.matchParent
+import org.jetbrains.anko.relativeLayout
+import org.jetbrains.anko.textView
+import org.jetbrains.anko.verticalLayout
+import org.jetbrains.anko.view
+import ru.dyatel.tsuschedule.R
+import ru.dyatel.tsuschedule.data.BaseLesson
+import ru.dyatel.tsuschedule.data.Lesson
+import ru.dyatel.tsuschedule.data.LessonType
+import ru.dyatel.tsuschedule.data.TeacherLesson
+import ru.dyatel.tsuschedule.utilities.hideIf
+
+abstract class BaseLessonView<in T : BaseLesson>(context: Context) : _LinearLayout(context) {
+
+    private companion object {
+        val timeViewId = View.generateViewId()
+        val auditoryViewId = View.generateViewId()
+        val disciplineViewId = View.generateViewId()
+    }
+
+    private val typeMarkerView: View
+    private val timeView: TextView
+    private val auditoryView: TextView
+    private val disciplineView: TextView
+
+    init {
+        lparams(width = matchParent) {
+            margin = dip(2)
+        }
+
+        typeMarkerView = view {
+        }.lparams(width = dip(4), height = matchParent) {
+            rightMargin = dip(4)
+        }
+
+        verticalLayout {
+            lparams(width = matchParent)
+
+            relativeLayout {
+                lparams(width = matchParent)
+
+                textView { id = timeViewId }.lparams {
+                    leftOf(auditoryViewId)
+                    alignParentLeft()
+                }
+                textView { id = auditoryViewId }.lparams {
+                    alignParentRight()
+                }
+            }
+
+            textView { id = disciplineViewId }
+
+            customize(this)
+        }
+
+        timeView = find(timeViewId)
+        auditoryView = find(auditoryViewId)
+        disciplineView = find(disciplineViewId)
+    }
+
+    protected abstract fun customize(container: _LinearLayout)
+
+    open fun bind(lesson: T) {
+        typeMarkerView.backgroundResource = when (lesson.type) {
+            LessonType.PRACTICE -> R.color.practice_color
+            LessonType.LECTURE -> R.color.lecture_color
+            LessonType.LABORATORY -> R.color.laboratory_color
+            LessonType.UNKNOWN -> R.color.unknown_color
+        }
+
+        val typeText = when (lesson.type) {
+            LessonType.PRACTICE -> R.string.lesson_type_practice
+            LessonType.LECTURE -> R.string.lesson_type_lecture
+            LessonType.LABORATORY -> R.string.lesson_type_laboratory
+            LessonType.UNKNOWN -> null
+        }?.let { context.getString(it) }
+
+        timeView.text = lesson.time
+        auditoryView.apply {
+            text = lesson.auditory
+            hideIf { text == null }
+        }
+        disciplineView.text = typeText?.let { "${lesson.discipline} ($typeText)" } ?: lesson.discipline
+    }
+
+    open fun unbind() {
+        timeView.text = null
+        auditoryView.text = null
+        disciplineView.text = null
+    }
+
+}
+
+class GroupLessonView(context: Context) : BaseLessonView<Lesson>(context) {
+
+    private lateinit var teacherView: TextView
+
+    override fun customize(container: _LinearLayout) {
+        container.apply {
+            teacherView = textView()
+        }
+    }
+
+    override fun bind(lesson: Lesson) {
+        super.bind(lesson)
+        teacherView.apply {
+            text = lesson.teacher
+            hideIf { text == null }
+        }
+    }
+
+    override fun unbind() {
+        teacherView.text = null
+        super.unbind()
+    }
+
+}
+
+class TeacherLessonView(context: Context) : BaseLessonView<TeacherLesson>(context) {
+
+    private lateinit var groupView: TextView
+
+    override fun customize(container: _LinearLayout) {
+        container.apply {
+            groupView = textView()
+        }
+    }
+
+    override fun bind(lesson: TeacherLesson) {
+        super.bind(lesson)
+        groupView.apply {
+            text = lesson.groups.joinToString(", ").takeUnless { it.isEmpty() }
+            hideIf { text == null }
+        }
+    }
+
+    override fun unbind() {
+        groupView.text = null
+        super.unbind()
+    }
+
+}
