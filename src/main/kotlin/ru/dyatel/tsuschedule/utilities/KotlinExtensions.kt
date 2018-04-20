865ca9f344d8888abf3fc910d916b4d925b6267f
sibwaf <7131069+sibwaf@users.noreply.github.com>
2018-04-20 22:42:31 +0300

    Remove useless NullableLateinit

--- src/main/kotlin/ru/dyatel/tsuschedule/utilities/KotlinExtensions.kt
+++ src/main/kotlin/ru/dyatel/tsuschedule/utilities/KotlinExtensions.kt
@@ -3,26 +3,7 @@ package ru.dyatel.tsuschedule.utilities
 import kotlinx.coroutines.experimental.CoroutineDispatcher
 import kotlinx.coroutines.experimental.Job
 import kotlinx.coroutines.experimental.cancelAndJoin
-import kotlin.properties.ReadWriteProperty
-import kotlin.reflect.KProperty
 import kotlinx.coroutines.experimental.launch as launchCoroutine
-
-class NullableLateinit<T> : ReadWriteProperty<Any, T?> {
-
-    private var initialized = false
-    private var value: T? = null
-
-    override fun getValue(thisRef: Any, property: KProperty<*>): T? {
-        if (initialized) return value
-        throw IllegalStateException("Property ${property.name} must be initialized before usage")
-    }
-
-    override fun setValue(thisRef: Any, property: KProperty<*>, value: T?) {
-        this.value = value
-        initialized = true
-    }
-
-}
 
 class ReplacingJobLauncher(private val dispatcher: CoroutineDispatcher) {
 
