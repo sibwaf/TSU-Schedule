e6902d7fc94aedd1e6e678d15abaeb652bcb1ee1
sibwaf <7131069+sibwaf@users.noreply.github.com>
2018-04-29 03:02:21 +0300

    Tweak ChangelogDao sorting and storage size

--- src/main/kotlin/ru/dyatel/tsuschedule/database/ChangelogDao.kt
+++ src/main/kotlin/ru/dyatel/tsuschedule/database/ChangelogDao.kt
@@ -3,13 +3,13 @@ package ru.dyatel.tsuschedule.database
 import android.content.ContentValues
 import android.database.sqlite.SQLiteDatabase
 import org.jetbrains.anko.db.MapRowParser
-import org.jetbrains.anko.db.SqlOrderDirection
 import org.jetbrains.anko.db.TEXT
 import org.jetbrains.anko.db.createTable
 import org.jetbrains.anko.db.delete
 import org.jetbrains.anko.db.dropTable
 import org.jetbrains.anko.db.select
 import ru.dyatel.tsuschedule.model.VersionChangelog
+import ru.dyatel.tsuschedule.updater.Release
 import ru.dyatel.tsuschedule.updater.ReleaseToken
 
 class ChangelogDao(databaseManager: DatabaseManager) : DatabasePart(databaseManager) {
@@ -49,8 +49,8 @@ class ChangelogDao(databaseManager: DatabaseManager) : DatabasePart(databaseMana
     fun request(): List<VersionChangelog> {
         return execute {
             select(TABLE)
-                    .orderBy(ChangelogColumns.VERSION, SqlOrderDirection.DESC)
                     .parseList(CHANGELOG_PARSER)
+                    .sortedByDescending { Release(it.version) }
         }
     }
 
@@ -58,15 +58,17 @@ class ChangelogDao(databaseManager: DatabaseManager) : DatabasePart(databaseMana
         executeTransaction {
             delete(TABLE)
 
-            releases.forEach {
-                val contentValues = ContentValues().apply {
-                    put(ChangelogColumns.VERSION, it.release.version)
-                    put(ChangelogColumns.PRERELEASE, it.prerelease.toInt())
-                    put(ChangelogColumns.CHANGES, it.changes)
-                }
+            releases.sortedBy { it.release }
+                    .takeLast(10)
+                    .forEach {
+                        val contentValues = ContentValues().apply {
+                            put(ChangelogColumns.VERSION, it.release.version)
+                            put(ChangelogColumns.PRERELEASE, it.prerelease.toInt())
+                            put(ChangelogColumns.CHANGES, it.changes)
+                        }
 
-                insert(TABLE, null, contentValues)
-            }
+                        insert(TABLE, null, contentValues)
+                    }
         }
     }
 
