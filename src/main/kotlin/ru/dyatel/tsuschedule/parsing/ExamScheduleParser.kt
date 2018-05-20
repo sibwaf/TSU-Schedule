1c6ed7846f823719f71bb0ca54fc0c8613c8e4a6
sibwaf <7131069+sibwaf@users.noreply.github.com>
2018-05-21 01:40:22 +0300

    Add exam information merging

--- src/main/kotlin/ru/dyatel/tsuschedule/parsing/ExamScheduleParser.kt
+++ src/main/kotlin/ru/dyatel/tsuschedule/parsing/ExamScheduleParser.kt
@@ -5,23 +5,53 @@ import org.jsoup.nodes.Element
 import ru.dyatel.tsuschedule.ParsingException
 import ru.dyatel.tsuschedule.model.Exam
 
+private data class Token(
+        val type: TokenType?,
+        val discipline: String,
+        val datetime: DateTime,
+        val auditory: String,
+        val teacher: String
+)
+
+private enum class TokenType {
+    CONSULTATION, EXAM
+}
+
 object ExamScheduleParser : ParserBase() {
 
     private val DATETIME_PATTERN = Regex("^(\\d+)\\.(\\d+)\\.(\\d+) (\\d+):(\\d+)$") // 11.06.18 09:00
 
+    private val TYPE_MAPPING = mapOf(
+            "конс" to TokenType.CONSULTATION,
+            "э" to TokenType.EXAM
+    )
+    private val TYPE_MARKER_PATTERN = Regex("\\(\\s*([А-Яа-я]+)\\.?\\s*\\)")
+
     fun parse(element: Element): Set<Exam> {
         return element.children()
                 .filter { it.tagName() == "div" }
-                .map {
-                    Exam(
-                            parseDiscipline(it),
-                            null,
-                            parseDatetime(it),
-                            null,
-                            parseAuditory(it),
-                            parseTeacher(it))
-                }
+                .map { parseSingle(it) }
+                .distinct()
+                .groupBy { it.discipline }
+                .flatMap { merge(it.value) }
                 .toSet()
+    }
+
+    private fun parseSingle(e: Element): Token {
+        var discipline = parseDiscipline(e)
+        val type = TYPE_MARKER_PATTERN.findAll(discipline)
+                .map { it to it.groupValues[1].toLowerCase() }
+                .singleOrNull { TYPE_MAPPING.containsKey(it.second) }
+                ?.let {
+                    discipline = discipline.removeRange(it.first.range).trim()
+                    TYPE_MAPPING[it.second]!!
+                }
+
+        val datetime = parseDatetime(e)
+        val auditory = parseAuditory(e)
+        val teacher = parseTeacher(e)
+
+        return Token(type, discipline, datetime, auditory, teacher)
     }
 
     private fun parseDatetime(e: Element): DateTime {
@@ -48,5 +78,43 @@ object ExamScheduleParser : ParserBase() {
 
     private fun parseTeacher(e: Element) =
             e.getElementsByClass("teac").requireSingle().text().trim()
+
+    private fun merge(tokens: Collection<Token>): Collection<Exam> {
+        val result = mutableListOf<Exam>()
+
+        val source = tokens.toMutableList()
+        val consultationToken = source.singleOrNull { it.type == TokenType.CONSULTATION }
+        val examToken = source.singleOrNull { it.type == TokenType.EXAM }
+
+        if (consultationToken != null && examToken != null) {
+            var teacher = examToken.teacher
+            if (consultationToken.teacher != examToken.teacher) {
+                teacher = "${consultationToken.teacher}, $teacher"
+            }
+
+            result += Exam(
+                    examToken.discipline,
+                    consultationToken.datetime,
+                    examToken.datetime,
+                    consultationToken.auditory,
+                    examToken.auditory,
+                    teacher)
+
+            source -= consultationToken
+            source -= examToken
+        }
+
+        for (token in source) {
+            result += Exam(
+                    token.discipline,
+                    null,
+                    token.datetime,
+                    null,
+                    token.auditory,
+                    token.teacher)
+        }
+
+        return result
+    }
 
 }
